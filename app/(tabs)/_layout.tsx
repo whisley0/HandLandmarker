2df327c3bb313f75c79a2dff41d930b236b5fc15
Edward Lam <whisley@hotmail.com>
2025-08-08 00:16:56 +0800

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,12 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: 'Camera',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera.fill" color={color} />,
+        }}></Tabs.Screen>
     </Tabs>
   );
 }
---
+++ app/(tabs)/camera.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { Linking, Platform, Text, } from 'react-native';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission
+} from 'react-native-vision-camera';
+
+export default function MyCameraScreen() {
+  const device = useCameraDevice('back');
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission().then((granted) => {
+        if (!granted) {
+          // User denied permission, guide them to settings
+          Linking.openSettings();
+        }
+      });
+    }
+  }, [hasPermission, requestPermission]);
+
+  if (!device) {
+    return <Text>No camera device found.</Text>;
+  }
+
+  const pixelFormat = Platform.OS === 'ios' ? 'rgb' : 'yuv';
+
+  return (
+    <Camera
+      device={device}
+      isActive={true}
+      style={{ flex: 1 }}
+    />
+  );
+}
+
